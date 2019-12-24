79a1d4ad72ed8beeaa25565109d7d502c3b1b660
Nurmatov-07 <55455911+Nurmatov-07@users.noreply.github.com>
2019-12-24 16:07:52 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ps -eo euid,ruid,comm |tail -n+2| awk '{if ($1 != $2) print $3}'
+ps -eo euid,ruid,comm |tail -n+2
+exec 0<temp.out
+while read euid ruid comm 
+do
+	if [[ $euid != $ruid ]]; then
+		echo "process $comm's euid differs from ruid"
+	fi
+done
+rm temp.out
